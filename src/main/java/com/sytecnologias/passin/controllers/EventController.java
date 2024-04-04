c92d8be42633d07b1bb1bbf9dd5432af8acf0262
Yuri Jivago <yurijivagoads@gmail.com>
2024-04-04 12:49:38 -0300

    Criação dos endpoints do Event - GET e POST

--- src/main/java/com/sytecnologias/passin/controllers/EventController.java
+++ src/main/java/com/sytecnologias/passin/controllers/EventController.java
@@ -1,13 +1,14 @@
 package com.sytecnologias.passin.controllers;
 
+import com.sytecnologias.passin.dto.EventIdDTO;
+import com.sytecnologias.passin.dto.EventRequestDTO;
+import com.sytecnologias.passin.dto.EventResponseDTO;
 import com.sytecnologias.passin.services.EventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/events")
@@ -15,9 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class EventController {
     private final EventService eventService;
     @GetMapping("/{eventId}")
-    public ResponseEntity<String> getEvent(@PathVariable String eventId){
-        this.eventService.getEventDetail(eventId);
-        return ResponseEntity.ok("Event controller -> Sucesso");
+    public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String eventId){
+        EventResponseDTO event = this.eventService.getEventDetail(eventId);
+        return ResponseEntity.ok(event);
+    }
+
+    @PostMapping
+    public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body, UriComponentsBuilder uriComponentsBuilder){
+        EventIdDTO eventIdDTO = this.eventService.createEvent(body);
+        var uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventIdDTO.eventId()).toUri();
+        return ResponseEntity.created(uri).body(eventIdDTO);
     }
 
 
