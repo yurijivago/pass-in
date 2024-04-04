0d2fa0979b7864f747d9c7d40cf5febdd363e443
Yuri Jivago <yurijivagoads@gmail.com>
2024-04-04 10:24:05 -0300

    Attendees class

---
+++ src/main/java/com/sytecnologias/passin/domain/attendee/Attendees.java
@@ -0,0 +1,35 @@
+package com.sytecnologias.passin.domain.attendee;
+
+import com.sytecnologias.passin.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "attendees")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Attendees {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
